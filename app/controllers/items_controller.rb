01e94ed31c4b79c73267b5db79fb75d1f33a8d4a
skarwaaaa <skarwaaaa@gmail.com>
2020-09-03 18:26:47 +0900

    商品投稿機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,15 +6,32 @@ class ItemsController < ApplicationController
     end
 
   def new
+    @item=Item.new
     # if current_user == null
     #   redirect_to ("/new/item")
     # end
   end
+  
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render new_item_path
+    end
+  end
 
-  def move_to_index
+    def move_to_index
     unless user_signed_in?
       redirect_to action: :new
     end
   end
 
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :image, :price, :category_id, 
+    :status_id, :delivery_fee_id, :prefecture_id, :shipping_date_id,).merge(user_id: current_user.id)
+    
+  end
 end
--- db/migrate/20200819155854_create_items.rb
+++ db/migrate/20200819155854_create_items.rb
@@ -5,12 +5,12 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.text :description, null: false
       t.string :image, null: false
       t.integer :price, null: false
-      t.integer :user_id, foreign_key: true, null: false
       t.integer :category_id, null: false
       t.integer :status_id, null: false
       t.integer :delivery_fee_id, null: false
       t.integer :prefecture_id, null: false
       t.integer :shipping_date_id, null: false
+      t.integer :user_id, foreign_key: true,
       t.timestamps
     end
   end
